f30de28f28579e7c6187278bb6dde21cf383e917
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-04 15:44:09 +0100

    finished all functions for operations

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand:payload.digit,
+          overwrite:false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -26,9 +33,46 @@ function reducer(state, { type, payload }) {
       }
     case ACTIONS.CLEAR:
       return {}
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.lenght === 1) {
+        return {
+          ...state,
+          currentOperand: null
+        }
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      }
+
+    case ACTIONS.EVALUATE:
+      if (state.operation == null || state.currentOperand == null || state.previusOperand == null) {
+        return state
+      }
+      return {
+        ...state,
+        overwrite: true,
+        previusOperand: null,
+        operation: null,
+        currentOperand:evaluate(state),
+      }
     case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previusOperand == null) {
         return state
+      }
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation
+        }
       }
       if (state.previusOperand == null) {
         return {
@@ -86,7 +130,7 @@ function App() {
           <div className="current-operand">{currentOperand}</div>
         </div>
         <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-        <button>DEL</button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
         <OperationButton operation="÷" dispatch={dispatch}/>
         <DigitButton digit="1" dispatch={dispatch}/>
         <DigitButton digit="2" dispatch={dispatch}/>
@@ -102,7 +146,8 @@ function App() {
         <OperationButton operation="-" dispatch={dispatch}/>
         <DigitButton digit="." dispatch={dispatch}/>
         <DigitButton digit="0" dispatch={dispatch}/>
-        <button className="span-two">=</button>
+        <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
+
       </div>
     </div>
   )
